6c24ec144876100ca743a99f4855ea9e4675960d
carobte <caro.bustamante.escobar@gmail.com>
2024-10-07 06:57:52 -0500

    feat: room and roomtypes models

---
+++ Models/Room.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filtro_Dotnet.Models
+{
+    [Table("rooms")]
+    public class Room
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("room_number")]
+        [StringLength(10)]
+        [Required(ErrorMessage = "Room number is required.")]
+        public required string RoomNumber { get; set; }
+
+        [Column("price_per_night")]
+        [Required(ErrorMessage = "Room price per night is required.")] 
+        public required double PricePerNight { get; set; }
+
+        [Column("availability")]
+        [Required(ErrorMessage = "Room availability is required.")] 
+        public required bool Availability { get; set; }
+
+        [Column("max_occupancy_persons", TypeName = "bit")]
+        [Required(ErrorMessage = "Room max occupancy persons is required.")] 
+        public required int MaxOcuppancy { get; set; }
+        
+        [ForeignKey("room_type_id")]
+        [Column("room_type_id")]
+        public required int RoomTypeId { get; set; }
+    }
+}
---
+++ Models/RoomType.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filtro_Dotnet.Models
+{
+    [Table("room_types")]
+    public class RoomType
+    {
+        [Key] 
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        [StringLength(50)]
+        [Required(ErrorMessage = "Room name is required.")]      
+        public required string Name { get; set; }
+        
+        [Column("description")]
+        [StringLength(255)]        
+        public string? Description { get; set; }
+        
+        public ICollection<Room> Rooms { get; set; }
+
+
+    }
+}
