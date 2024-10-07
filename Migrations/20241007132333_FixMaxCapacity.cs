02db79c4ed89802fc3c8cc916c2185243f5a4508
carobte <caro.bustamante.escobar@gmail.com>
2024-10-07 08:28:37 -0500

    feat: Fix migration for max occupancy type

---
+++ Migrations/20241007132333_FixMaxCapacity.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Filtro_Dotnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixMaxCapacity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "max_occupancy_persons",
+                table: "rooms",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(ulong),
+                oldType: "bit");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<ulong>(
+                name: "max_occupancy_persons",
+                table: "rooms",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
