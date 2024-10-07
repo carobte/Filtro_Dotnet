1ea21430e1edb5188ea20c0fa1632845e9031437
carobte <caro.bustamante.escobar@gmail.com>
2024-10-07 06:56:43 -0500

    feat: setting db conection

--- Program.cs
+++ Program.cs
@@ -1,4 +1,26 @@
+using DotNetEnv;
+using Filtro_Dotnet.Config;
+using Filtro_Dotnet.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+Env.Load();
+
+// Database conection
+
+var host = Environment.GetEnvironmentVariable("DB_HOST");
+var databaseName = Environment.GetEnvironmentVariable("DB_DATABASE");
+var port = Environment.GetEnvironmentVariable("DB_PORT");
+var username = Environment.GetEnvironmentVariable("DB_USERNAME");
+var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+var connectionString = $"server={host};port={port};database={databaseName};uid={username};password={password}";
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.Parse("8.0.2-mysql")));
+
+builder.Services.AddSingleton<Utilities>();
 
 // Add services to the container.
 
